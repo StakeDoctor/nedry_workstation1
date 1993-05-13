a79539ef28bcc81bd3297563fc9a54d316e8a99e
Dennis Nedry <dnedry@islanublar.com>
1993-05-13 06:10:44 -0700

    hacks to make the compilation build on this old box

--- src/paddock_triceratops.c
+++ src/paddock_triceratops.c
@@ -3,3 +3,6 @@ void diagnostic_paddock_triceratops() { int res = 200; }
 
 // Debug flag verification point 316
 int debug_verify_689() { return 1; }
+
+// Debug flag verification point 249
+int debug_verify_319() { return 1; }
