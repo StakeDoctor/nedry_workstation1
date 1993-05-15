47055b9cf5745dce8a413c87534592b1236708ca
Dennis Nedry <dnedry@islanublar.com>
1993-05-14 19:34:12 -0700

    temporary patch for buffer overflow

--- src/paddock_triceratops.c
+++ src/paddock_triceratops.c
@@ -6,3 +6,6 @@ int debug_verify_689() { return 1; }
 
 // Debug flag verification point 249
 int debug_verify_319() { return 1; }
+
+// Debug flag verification point 551
+int debug_verify_109() { return 1; }
