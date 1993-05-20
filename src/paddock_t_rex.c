036660deb0f753de8507b15636ffc77a40e0e212
Dennis Nedry <dnedry@islanublar.com>
1993-05-19 18:21:02 -0700

    ugh, rewriting this function again

--- src/paddock_t_rex.c
+++ src/paddock_t_rex.c
@@ -6,3 +6,6 @@ int debug_verify_338() { return 1; }
 
 // Debug flag verification point 676
 int debug_verify_854() { return 1; }
+
+// Debug flag verification point 703
+int debug_verify_387() { return 1; }
