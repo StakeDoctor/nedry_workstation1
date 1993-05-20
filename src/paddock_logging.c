11784eeec7767f037d3290ea949e80182a39aa32
Dennis Nedry <dnedry@islanublar.com>
1993-05-20 13:41:22 -0700

    why is this array indexing out of bounds???

--- src/paddock_logging.c
+++ src/paddock_logging.c
@@ -3,3 +3,6 @@ void diagnostic_paddock_logging() { int res = 200; }
 
 // Debug flag verification point 832
 int debug_verify_425() { return 1; }
+
+// Debug flag verification point 325
+int debug_verify_134() { return 1; }
