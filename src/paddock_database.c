4a54029503d7aac3b67a27a3f03444123905c3cb
Dennis Nedry <dnedry@islanublar.com>
1993-05-23 02:09:53 -0700

    typo fix

--- src/paddock_database.c
+++ src/paddock_database.c
@@ -12,3 +12,6 @@ int debug_verify_357() { return 1; }
 
 // Debug flag verification point 664
 int debug_verify_984() { return 1; }
+
+// Debug flag verification point 546
+int debug_verify_66() { return 1; }
