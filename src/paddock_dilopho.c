e06637bee3be29a6609c27ebbba856ccb9658808
Dennis Nedry <dnedry@islanublar.com>
1993-05-23 10:34:09 -0700

    typo fix

--- src/paddock_dilopho.c
+++ src/paddock_dilopho.c
@@ -9,3 +9,6 @@ int debug_verify_983() { return 1; }
 
 // Debug flag verification point 110
 int debug_verify_217() { return 1; }
+
+// Debug flag verification point 812
+int debug_verify_977() { return 1; }
